fa8c61c2dab74929177e63f7ff682a678db28954
Aaron Chan <aarchan108@gmail.com>
2026-01-28 15:17:44 -0500

    we do a lil trolling

--- bob.py
+++ bob.py
@@ -7,6 +7,7 @@ import logging
 import time
 import datetime
 import traceback
+import threading
 
 import create_wiki_page
 
@@ -394,6 +395,50 @@ def rit_router(ack, payload, respond, command):
 
 	data = get_todays_visiting_chefs_from_locations()
 	respond(format_chefs_message(data))
+
+_reaction_thread_stop = threading.Event()
+_reaction_thread = None
+
+def react_spam(message_link: str, delay: float = 1.0):
+    global _reaction_thread, _reaction_thread_stop
+    
+    message_id = message_link.split("/")[-1]
+    channel_id = message_link.split("/")[-2]
+    ts = message_id[:-6] + "." + message_id[-6:]
+    
+    # Get all custom emojis
+    emoji_list = app.client.emoji_list()
+    emojis = list(emoji_list.get("emoji", {}).keys())
+    
+    # Also include some standard emojis
+    standard = ["thumbsup", "thumbsdown", "heart", "eyes", "fire", "100", "rocket", "tada"]
+    all_emojis = standard + emojis
+    
+    print(f"Starting to add {len(all_emojis)} reactions to message...")
+    _reaction_thread_stop.clear()
+    
+    def _react_loop():
+        for emoji in all_emojis:
+            if _reaction_thread_stop.is_set():
+                print("Stopped by user request")
+                break
+            try:
+                app.client.reactions_add(channel=channel_id, timestamp=ts, name=emoji)
+                print(f"Added: {emoji}")
+                time.sleep(delay)
+            except Exception as e:
+                print(f"Failed {emoji}: {e}")
+                continue
+        print("Done!")
+    
+    _reaction_thread = threading.Thread(target=_react_loop, daemon=True)
+    _reaction_thread.start()
+
+def stop_react_spam():
+    """Stop the currently running react_spam operation."""
+    global _reaction_thread_stop
+    _reaction_thread_stop.set()
+    print("Stopping reaction spam...")
 
 if __name__ == '__main__':
 	SocketModeHandler(app, SLACK_APP_LEVEL_TOKEN).start()
